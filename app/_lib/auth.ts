c417b78a36ce39449185732a4a4eee4c44f6e304
fsjavier <javier.fersal@gmail.com>
2024-08-17 09:07:34 +0200

    Setup authentication

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import authConfig from "./auth.config";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { firestore } from "./firestore";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: FirestoreAdapter(firestore),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
