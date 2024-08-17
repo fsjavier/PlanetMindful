9ff696bd92afd02bd4c97ab0f473ccb9a5d2d2dd
fsjavier <javier.fersal@gmail.com>
2024-08-17 09:08:10 +0200

    Enable login and logout

--- app/_components/SigninButton.tsx
+++ app/_components/SigninButton.tsx
@@ -1,14 +1,19 @@
-import { signIn } from "@/app/_lib/auth";
+import { signInAction } from "../_lib/actions";
 
-export function SignIn() {
+function SignInButton() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn();
-      }}
-    >
-      <button type="submit">Sign in</button>
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
     </form>
   );
 }
+
+export default SignInButton;
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <div>Account</div>;
+import SignOutButton from "../_components/SignoutButton";
+import { auth } from "../_lib/auth";
+
+export const metadata = {
+  title: "Guest area",
+};
+
+export default async function Page() {
+  const session = await auth();
+
+  console.log(session);
+  const firstName: string = session?.user?.name?.split(" ").at(0) ?? "";
+  return (
+    <div className="flex justify-center flex-col">
+      <h2 className="font-semibold text-2xl text-tertiary-950 my-5 self-center">
+        {firstName.length > 1 ? `Welcome, ${firstName}` : "Welcome"}
+      </h2>
+      <SignOutButton />
+    </div>
+  );
 }
