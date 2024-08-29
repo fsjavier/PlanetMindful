b3d77aa856e00007d576c513d3f192f7159993a6
fsjavier <javier.fersal@gmail.com>
2024-08-29 16:55:29 +0200

    Check and create profile on sign in

--- app/_components/SigninButton.tsx
+++ app/_components/SigninButton.tsx
@@ -1,6 +1,25 @@
-import { signInAction } from "../_lib/actions";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { getSession, signInAction } from "../_lib/actions";
+import { checkAndCreateUserProfile } from "../_services/data-service";
 
 function SignInButton() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkProfile = async () => {
+      const session = await getSession();
+      const userId: string | null | undefined = session?.user?.uid;
+      if (userId) {
+        await checkAndCreateUserProfile(userId);
+        router.push("/account");
+      }
+    };
+    checkProfile();
+  }, [router]);
+
   return (
     <form action={signInAction}>
       <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
