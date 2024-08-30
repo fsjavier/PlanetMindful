44931ce18f2d2fcda2a4bf1a05bf18af634550bf
fsjavier <javier.fersal@gmail.com>
2024-08-30 09:07:34 +0200

    Add methods to check, create and update profiles in firestore

--- app/_services/data-service.ts
+++ app/_services/data-service.ts
@@ -3,14 +3,27 @@ import {
   getDocs,
   doc,
   getDoc,
+  setDoc,
   DocumentData,
 } from "firebase/firestore";
 import { db } from "@/app/_lib/firebase";
+import { convertFirestoreData } from "@/app/_utils/firestore-helpers";
 
-interface User {
+export interface User {
   id: string;
   name: string;
+  image: string;
   email: string;
+}
+
+export interface Profile {
+  name: string;
+  email: string;
+  userId: string;
+  description?: string;
+  image: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export async function getUsers() {
@@ -35,9 +48,52 @@ export async function getUser(uid: string): Promise<User | null> {
     return {
       id: userDoc.id,
       name: data.name,
+      image: data.image,
       email: data.email,
     };
   } else {
-    return null; // User not found
+    return null;
   }
+}
+
+export async function checkAndCreateUserProfile(uid: string): Promise<Profile> {
+  const profileDocRef = doc(db, "profiles", uid);
+  const profileDoc = await getDoc(profileDocRef);
+
+  if (!profileDoc.exists()) {
+    const user = (await getUser(uid)) as User;
+    const profile: Profile = {
+      name: user.name,
+      email: user.email,
+      image: user.image,
+      userId: user.id,
+      createdAt: new Date(),
+    };
+
+    await setDoc(profileDocRef, profile);
+
+    return profile;
+  } else {
+    return profileDoc.data() as Profile;
+  }
+}
+
+export async function getProfile(userId: string): Promise<Profile> {
+  const profileDocRef = doc(db, "profiles", userId);
+  const profileDoc = await getDoc(profileDocRef);
+
+  if (!profileDoc.exists()) throw new Error("Profile does not exist");
+
+  const data = profileDoc.data();
+  const profile = {
+    name: data.name,
+    email: data.email,
+    userId: data.userId,
+    image: data.image,
+    description: data.description,
+    createdAt: data.createdAt,
+    updatedAt: data.updatedAt,
+  };
+
+  return convertFirestoreData(profile);
 }
